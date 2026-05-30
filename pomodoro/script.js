53c65ff7a42da46c0ffd27c7f13ef4bfa74f1952
Draguisada <draguisada@gmail.com>
2026-05-29 21:56:42 -0300

    menos de 5 minutos HAHAHAHA

---
+++ pomodoro/script.js
@@ -0,0 +1,51 @@
+const svg = document.getElementById("principal");
+const circulo = document.getElementById('circulo');
+
+const botao = document.getElementById('botao');
+const audio = document.getElementById('audio');
+
+window.addEventListener("resize", deixarTamanhoDaTela)
+
+function deixarTamanhoDaTela() {
+    console.log("mudou");
+    const newWidth = window.innerWidth;
+    const newHeight = window.innerHeight;
+
+    svg.width.baseVal.value = newWidth;
+    svg.height.baseVal.value = newHeight;
+};
+deixarTamanhoDaTela();
+
+let pomodoro = [25, 5, 25, 5, 25, 5, 25];
+let indice = 0;
+let iniciado = false;
+
+botao.addEventListener("click", (e) => {
+    if (iniciado) return;
+
+    iniciado = true;
+
+    nextTimer();
+
+
+
+
+});
+
+function nextTimer() {
+    botao.innerText = pomodoro[indice];
+    setTimeout(dispararAlarme, pomodoro[indice] * 300);
+    
+    ++indice;
+}
+
+function dispararAlarme() {
+    audio.play();
+    if (indice < pomodoro.length) {
+        nextTimer();
+    } else {
+        indice = 0;
+        botao.innerText = "Iniciar";
+        iniciado = false;
+    }
+}
